909f96d449f7c8a67dc4d8c0194cc2c7a7d0e73a
Shu Yan <yanshu.usc@gmail.com>
2015-02-09 00:38:43 -0500

    add svm with output

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,6 +27,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+svc = SVC(kernel = 'linear')
+
+t0 = time()
+svc.fit(features_train, labels_train)
+str1 = "training time: " + str(round(time()-t0, 3)) + "s"
+print str1
+
+t1 = time()
+labels_pred = svc.predict(features_test)
+str2 =  "predicting time: " + str(round(time()-t1, 3)) + "s"
+print str2
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_pred, labels_test)
+print accuracy
+
 #########################################################
 
+out = open('output.txt', 'w')
 
+out.write('sklearn.svm.SVC(kernel = \'linear\')\n')
+out.write(str1 + '\n')
+out.write(str2 + '\n')
+out.write('Accuracy: ' + str(accuracy))
+
+out.close()
